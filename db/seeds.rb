77d30bdde318f7477708cb775603f915dcb270b2
Nicholo Dominguez <jncdominguez@gmail.com>
2023-11-27 14:42:25 +0800

    Rails installation

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Blog.create([
+    {title: "First Blog", body: "This is the first blog"},
+    {title: "Second Blog", body: "This is the second blog", subheader: "A sequel to the first one"},
+    {title: "Third Blog", body: "This is the third blog"},
+    {title: "Fourth Blog", body: "This is the fourth blog"},
+])
